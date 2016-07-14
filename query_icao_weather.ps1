902879a6045f9ad046664003e6dbbba16ddf4431
David O'Brien <me@david-obrien.net>
2016-07-14 14:51:39 +1000

    Add parameter to query script to be more flexible.

--- query_icao_weather.ps1
+++ query_icao_weather.ps1
@@ -1,5 +1,10 @@
+param (
+  [Parameter(mandatory)]
+  [string]$icao_code
+)
+
 $body = @{
-    icao = 'eddk'
+    icao = $icao_code
 }
 
 $res = Invoke-WebRequest -Uri https://dotest.azurewebsites.net/api/metar -Method Post -Body (ConvertTo-Json $body) -Verbose
