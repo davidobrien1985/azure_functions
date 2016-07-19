5bd4dbf6fd20f222aaaad456ea3c5ed281b928a7
David O'Brien <me@david-obrien.net>
2016-07-19 23:37:03 +1000

    Use internal query string param.

--- icao_weather.ps1
+++ icao_weather.ps1
@@ -1,6 +1,6 @@
-$request = Get-Content $req -Raw | ConvertFrom-Json
+#$request = Get-Content $req -Raw | ConvertFrom-Json
 
-$res = Invoke-RestMethod -Method Get -Uri "http://avwx.rest/api/metar.php?station=$($request.icao)&format=JSON" -verbose
+$res = Invoke-RestMethod -Method Get -Uri "http://avwx.rest/api/metar.php?station=$req_query_icao&format=JSON" -verbose
 
 $res = $res.'Raw-Report'
 
